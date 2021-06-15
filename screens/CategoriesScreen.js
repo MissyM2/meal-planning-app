c1d822598dce8a0e714497efab5e33913ce26ba7
Missy Maloney <missymaloney1@gmail.com>
2021-06-15 11:58:20 -0400

    added dummy data; nav and spassing header data from screen to screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,28 +1,66 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { 
+    View, 
+    Text, 
+    FlatList, 
+    StyleSheet, 
+    TouchableOpacity,
+    Platform
+} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
 
 const CategoriesScreen = props => {
-    console.log(props);
 
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMeals', params: {
+                    categoryId: itemData.item.id
+                } 
+            });
+            }}>
+                <View style={styles.gridItem}>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+    
     return(
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-            <Button title="Go to Meals!" onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals'})
-            }}/>
-        </View>
-    )
+        <FlatList 
+            data={CATEGORIES} 
+            renderItem={renderGridItem} 
+            numColumns={2} 
+        />
+    );
 };
 
-// could also use replace() with props.navigation.replace('screenName'): 
-//no back button and no forward automation -- login screen where
-// user shouldn't be able to go back to login screen
+// a React function/constant is just a Javascript object so we are able to add a property
+// these properties will be respected by react navigation
+
+CategoriesScreen.navigationOptions = {
+    title: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
+
+
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id == catId)
     return(
         <View style={styles.screen}>
             <Text>The Category MealScreen</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go to Meal Details!" onPress={() => {
                 props.navigation.navigate({
                     routeName: 'MealDetail'
@@ -16,6 +23,20 @@ const CategoryMealScreen = props => {
             }} />
         </View>
     )
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+    const catId = navigationData.navigation.getParam('categoryId');
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    return {
+        title: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    
+    };
+
 };
 
 // can also use 'push' and don't need an object: i.e. if you are in a Folder 
