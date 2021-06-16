7f7eef3047231ccc610d21fb3483eaeb294b13e4
Missy Maloney <missymaloney1@gmail.com>
2021-06-16 12:46:01 -0400

    more stacknavigation, adding buttons to header

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -37,7 +37,10 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        elevation: 5,
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+
+
     },
     container: {
         flex: 1,
@@ -46,7 +49,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
