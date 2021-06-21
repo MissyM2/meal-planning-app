45e494dab56cbb1e21a6ba24d9dea207f765bd4c
Missy Maloney <missymaloney1@gmail.com>
2021-06-21 19:47:22 -0400

    finished favorites section

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react';
-import {
-  ScrollView,
-  View,
-  Image,
-  Text,
-  StyleSheet
-} from 'react-native';
+import React, { useEffect, useCallback } from 'react';
+import { ScrollView, View, Image, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
+import { toggleFavorite } from '../store/actions/meals';
 
 const ListItem = props => {
   return (
@@ -21,14 +16,31 @@ const ListItem = props => {
 };
 
 const MealDetailScreen = props => {
-    const availableMeals = useSelector(state => state.meals.meals);
-    const mealId = props.navigation.getParam('mealId');
-    
-    const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+  const availableMeals = useSelector(state => state.meals.meals);
+  
+  const mealId = props.navigation.getParam('mealId');
+  const currentMealIsFavorite = useSelector(state => 
+    state.meals.favoriteMeals.some(meal => meal.id === mealId)
+  );
+  console.log('is current meal a favorite?', currentMealIsFavorite);
 
-    //useEffect(() => {
-    //    props.navigation.setParams({ mealTitle: selectedMeal.title });
-    //}, [selectedMeal]);
+  const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  const dispatch = useDispatch();
+
+  const toggleFavoriteHandler = useCallback(() => {
+    console.log('made it to toggleFavoriteHandler');
+    dispatch(toggleFavorite(mealId));
+  }, [dispatch, mealId]);
+
+  useEffect(() => {
+    // props.navigation.setParams({ mealTitle: selectedMeal.title });
+    props.navigation.setParams({toggleFav: toggleFavoriteHandler});
+  }, [toggleFavoriteHandler]);
+
+  useEffect(() => {
+    props.navigation.setParams({isFav: currentMealIsFavorite});
+  }, [currentMealIsFavorite]);
 
   return (
     <ScrollView>
@@ -51,19 +63,20 @@ const MealDetailScreen = props => {
 };
 
 MealDetailScreen.navigationOptions = navigationData => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  mealTitle = navigationData.navigation.getParam('mealTitle');
-  //const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  // const mealId = navigationData.navigation.getParam('mealId');
+  const mealTitle = navigationData.navigation.getParam('mealTitle');
+  const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  const isFavorite = navigationData.navigation.getParam('isFav');
+
+  // const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Favorite"
-          iconName="ios-star"
-          onPress={() => {
-            console.log('Mark as favorite!');
-          }}
+          iconName={isFavorite ? 'ios-star' : 'ios-star-outline'}
+          onPress={toggleFavorite}
         />
       </HeaderButtons>
     )
