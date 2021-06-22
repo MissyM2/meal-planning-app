5547e18d0af9cecd981bc32b33af969ccff69809
Missy Maloney <missymaloney1@gmail.com>
2021-06-22 13:00:51 -0400

    add filters functionality

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View, StyleSheet} from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealScreen = props => {
     
@@ -13,6 +15,14 @@ const CategoryMealScreen = props => {
     const displayedMeals = availableMeals.filter(
         meal => meal.categoryIds.indexOf(catId) >= 0
     );
+
+    if (displayedMeals.length === 0) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>No meals found, maybe check your filters?</DefaultText>
+            </View>
+        )
+    }
     return <MealList listData={displayedMeals} navigation={props.navigation}/>
 };
 
@@ -30,5 +40,12 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
 // and want to move to another folder
 // if you want to save and return to previous screen:  create a button with goBack()
 
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default CategoryMealScreen;
